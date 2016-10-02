0667ff42a47c67ac8f5839e1cb3a190b94f70b8e
Budi Rahardjo <rahard@gmail.com>
2016-10-02 21:16:55 +0700

    add button stuff

---
+++ button/README.md
@@ -0,0 +1,6 @@
+Button-related codes
+
+simple-buttons: testing ProcodeCG Key board, whihc has 4 buttons
+
+Budi Rahardjo
+* twitter: @rahard
---
+++ button/src/simple-buttons.ino
@@ -0,0 +1,30 @@
+/*
+simple buttons tester
+
+there are 4 buttons in procodecg key board
+read each one of them, and send them to the LEDs
+no debounce yet. just straight through
+
+2016, public domain
+Budi Rahardjo (@rahard)
+*/
+
+// pin configuration for Arduino UNU board + ProcodeCG LED
+int led[] = {7, 6, 5, 4, 3, 2};
+int key[] = {11, 10, 9, 8};
+int but[] = {0, 0, 0, 0, 0, 0};
+int NUMLED=6;
+int NUMKEY=4;
+int i;
+
+void setup() {
+   for (i=0; i<NUMKEY; i++) { pinMode(key[i], INPUT); }
+   for (i=0; i<NUMLED; i++) { pinMode(led[i], OUTPUT); }
+}
+
+void loop() {
+   // read buttons/keys
+   for (i=0; i<NUMKEY; i++) { but[i]=digitalRead(key[i]); }
+   // send them to LEDs directly, no debounce
+   for (i=0; i<NUMLED; i++) { digitalWrite(led[i], but[i]); }
+}
