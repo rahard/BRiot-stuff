4dea58fcfd33fa26d8849770914bca5b9cda3519
luqmanr <luqman.rahardjo@gmail.com>
2023-09-18 10:41:19 +0700

    Update klien-paho.py

--- MQTT/klien-paho.py
+++ MQTT/klien-paho.py
@@ -3,17 +3,15 @@ import paho.mqtt.client as paho
 
 def on_connect(client, userdata, flags, rc):
 	print("Connected with result code "+str(rc))
-	client.subscribe("led1")
+	client.subscribe("inTopic")
 
 def on_message(client, userdata, msg):
-	print(msg.topic+" "+str(msg.payload))
+	print(f'topic: {msg.topic} - msg: {msg.payload.decode()}')
 
 client = paho.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-#client.connect("192.168.4.242", 1883, 60)
-client.connect("192.168.43.67", 1883, 60)
+client.connect("mqtt.luqmanr.xyz", 1883, 60)
 
 client.loop_forever()
-
