55794aa3ae03eec3af2c3e38b3fd4048bbdb514f
Luqman Rahardjo <luqman.rahardjo@gmail.com>
2023-09-18 14:06:10 +0700

    else if

--- MQTT/src/mqtt_esp8266.ino
+++ MQTT/src/mqtt_esp8266.ino
@@ -75,7 +75,7 @@ void callback(char* topic, byte* payload, unsigned int length) {
     digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
     // but actually the LED is on; this is because
     // it is acive low on the ESP-01)
-  } else {
+  } else if ((char)payload[0] == '0') {
     digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
   }
 
