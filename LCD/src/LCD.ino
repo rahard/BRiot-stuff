b10f3063254e1e29da58f929da59541590e515a3
Budi Rahardjo <rahard@gmail.com>
2024-09-08 11:25:14 +0700

    update minor
    nambahkan sensor

--- LCD/src/LCD.ino
+++ LCD/src/LCD.ino
@@ -6,8 +6,8 @@
 // SDA -> D1
 // SCL -> D2
 
-LiquidCrystal_I2C lcd(0x27,16,2);  
 // set the LCD address to 0x3F or 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x27,16,2);  
 
 void setup() {
   lcd.init();
@@ -16,7 +16,7 @@ void setup() {
   
   // Print a message on both lines of the LCD.
   lcd.setCursor(2,0);   //Set cursor to character 2 on line 0
-  lcd.print("Hello world!");
+  lcd.print("Hellow world!");
   
   lcd.setCursor(1,1);   //Move cursor to character 2 on line 1
   lcd.print("Budi Rahardjo");
