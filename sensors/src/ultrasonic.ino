38494009b9f546c0e2f6f15797f9dc257d8f4040
Budi Rahardjo <rahard@gmail.com>
2024-10-22 09:13:17 +0700

    Auto stash before merge of "master" and "origin/master"

---
+++ sensors/src/ultrasonic.ino
@@ -0,0 +1,30 @@
+const int trigPin = D5;
+const int echoPin = D6;   
+long duration;  
+int distance;  
+
+void setup() {  
+   pinMode(trigPin, OUTPUT); 
+   pinMode(echoPin, INPUT); 
+   Serial.begin(9600); // 
+} 
+
+void loop() {  
+   digitalWrite(trigPin, LOW);  
+   delayMicroseconds(2);  
+   // Sets the trigPin on HIGH state for 10 micro seconds  
+   digitalWrite(trigPin, HIGH);  
+   delayMicroseconds(10);  
+   digitalWrite(trigPin, LOW);  
+
+   // Reads the echoPin, returns the sound wave travel time in microseconds  
+   duration = pulseIn(echoPin, HIGH);  
+   // Calculating the distance  
+   distance= duration*0.034/2;  
+
+   // Prints the distance on the Serial Monitor  
+   Serial.print("Distance: ");  
+   Serial.println(distance);  
+   delay(100);  
+ }  
+
